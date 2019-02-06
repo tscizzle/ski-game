2d8ad02164a374eaa01c91e6c686f10e898dea08
Tyler S-C <tscizzle@gmail.com>
2019-02-06 01:12:08 -0500

    added walls

--- src/game.js
+++ src/game.js
@@ -4,6 +4,9 @@ import Phaser from 'phaser';
 class SkiSlope extends Phaser.Scene {
   constructor() {
     super('SkiSlope');
+    this.wallLeft = null;
+    this.wallRight = null;
+    this.wallGraphics = null;
     this.distanceMarker0 = null;
     this.distanceMarker1 = null;
     this.skiPlayer = null;
@@ -17,12 +20,19 @@ class SkiSlope extends Phaser.Scene {
   }
 
   create() {
-    // SPRITES
-    this.distanceMarker0 = this.add.text(100, 400, '0', {
+    // VISUAL OBJECTS
+    this.wallLeft = new Phaser.Geom.Line(-300, -100000, -300, 100000);
+    this.wallRight = new Phaser.Geom.Line(300, -100000, 300, 100000);
+    this.wallGraphics = this.add.graphics({
+      lineStyle: { color: 0x000000 },
+    });
+    this.wallGraphics.strokeLineShape(this.wallLeft);
+    this.wallGraphics.strokeLineShape(this.wallRight);
+    this.distanceMarker0 = this.add.text(-250, 0, '0', {
       fontSize: '32px',
       fill: '#000',
     });
-    this.distanceMarker1 = this.add.text(100, -100, '500', {
+    this.distanceMarker1 = this.add.text(-250, -500, '500', {
       fontSize: '32px',
       fill: '#000',
     });
@@ -55,14 +65,16 @@ class SkiSlope extends Phaser.Scene {
       this.skiPlayer.y += Math.cos(this.skiPlayer.rotation) * 2;
     }
 
-    // PLACE DISTANCE MARKER
+    // DRAW DISTANCE MARKERS
     const skiPlayerProgress = -this.skiPlayer.y;
     const closest500Below = Math.floor(skiPlayerProgress / 500) * 500;
     const closest500Above = Math.ceil(skiPlayerProgress / 500) * 500;
-    this.distanceMarker0.y = -closest500Below;
-    this.distanceMarker0.setText(String(closest500Below));
-    this.distanceMarker1.y = -closest500Above;
-    this.distanceMarker1.setText(String(closest500Above));
+    if (this.distanceMarker0.y !== -closest500Below) {
+      this.distanceMarker0.y = -closest500Below;
+      this.distanceMarker0.setText(String(closest500Below));
+      this.distanceMarker1.y = -closest500Above;
+      this.distanceMarker1.setText(String(closest500Above));
+    }
   }
 
   /* HELPERS */
