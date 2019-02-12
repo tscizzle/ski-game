e29ee7bf0e93ac437aff9f7ce766cc070b7b82cc
Tyler S-C <tscizzle@gmail.com>
2019-02-12 18:45:16 -0500

    Updates

---
+++ precache-manifest.278b1a0b7197334649badc9f99cdacc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a885b2e4a80be59a127a",
+    "url": "/ski-game/static/css/main.72a368f3.chunk.css"
+  },
+  {
+    "revision": "a885b2e4a80be59a127a",
+    "url": "/ski-game/static/js/main.a885b2e4.chunk.js"
+  },
+  {
+    "revision": "488d0b8abd9a68da4f1f",
+    "url": "/ski-game/static/js/1.488d0b8a.chunk.js"
+  },
+  {
+    "revision": "e1e6cde609ddb233bfc6",
+    "url": "/ski-game/static/js/runtime~main.e1e6cde6.js"
+  },
+  {
+    "revision": "ac197344ebd35cf6c24ecbbfb2c1740c",
+    "url": "/ski-game/index.html"
+  }
+];
