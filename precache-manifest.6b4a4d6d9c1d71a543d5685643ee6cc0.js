120753981da1715789e4aa58bb3052f6f5b6809d
Tyler S-C <tscizzle@gmail.com>
2019-02-12 23:36:10 -0500

    Updates

---
+++ precache-manifest.6b4a4d6d9c1d71a543d5685643ee6cc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72707cc7b2b0de741b0a",
+    "url": "https://tscizzle.github.io/ski-game/static/css/main.72a368f3.chunk.css"
+  },
+  {
+    "revision": "72707cc7b2b0de741b0a",
+    "url": "https://tscizzle.github.io/ski-game/static/js/main.72707cc7.chunk.js"
+  },
+  {
+    "revision": "488d0b8abd9a68da4f1f",
+    "url": "https://tscizzle.github.io/ski-game/static/js/1.488d0b8a.chunk.js"
+  },
+  {
+    "revision": "84780ba91a4a0727fe56",
+    "url": "https://tscizzle.github.io/ski-game/static/js/runtime~main.84780ba9.js"
+  },
+  {
+    "revision": "cf599e46ca3c0e70e53ed16d233db27e",
+    "url": "https://tscizzle.github.io/ski-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ski-game/precache-manifest.63a648837023b4a076011f2291f1c7b6.js"
+  "https://tscizzle.github.io/ski-game/precache-manifest.6b4a4d6d9c1d71a543d5685643ee6cc0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ski-game/index.html", {
+workbox.routing.registerNavigationRoute("https://tscizzle.github.io/ski-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
