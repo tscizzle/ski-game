a44e2dbef542f112056f492642437e6056a9559d
Tyler S-C <tscizzle@gmail.com>
2019-02-12 23:48:06 -0500

    removed debugging console, it works on github pages

--- src/game.js
+++ src/game.js
@@ -7,7 +7,6 @@ class SkiSlope extends Phaser.Scene {
   }
 
   preload() {
-    console.log(this.publicURL('/gameAssets/images/racingArrows.png'));
     this.load.image(
       'arrows',
       this.publicURL('/gameAssets/images/racingArrows.png')
